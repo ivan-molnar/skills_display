3355abc789c7aa2b45ab3211f1817c2a063dcb82
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-05-19 09:43:44 +0200

    popup version

--- calc.js
+++ calc.js
@@ -1,73 +1,86 @@
-notes = JSON.parse(localStorage.getItem("notes"));
+chrome.storage.sync.get("notes", ({
+    notes
+}) => {
+    localStorage.setItem('note', notes);
+});
+let json = localStorage.getItem("note");
+
+let notes = JSON.parse(json);
 tree = []
 
 perfectScore = 95;
 
 const equals = (a, b) =>
-  a.length === b.length &&
-  a.every((v, i) => v === b[i]);
+    a.length === b.length &&
+    a.every((v, i) => v === b[i]);
 
-compare = function(a, b){
+compare = function (a, b) {
     a = a.Activity[0];
     b = b.Activity[0];
 
-    for(let i=0; i<Math.max(a.length, b.length); i++){
-        if(a[i].localeCompare(b[i])!= 0){
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+        if (a[i].localeCompare(b[i]) != 0) {
             return a[i].localeCompare(b[i]);
         }
 
-        if(a.length-1==i){ return -1; }
-        if(b.length-1==i){ return 1; }
+        if (a.length - 1 == i) {
+            return -1;
+        }
+        if (b.length - 1 == i) {
+            return 1;
+        }
     }
 }
 
-for(i in notes){
+for (i in notes) {
     note = notes[i];
     note.Percentage = parseInt(note.Percentage)
     note.Activity[0] = note.Activity[0].split("-");
-    note.Max = note.Points/note.Percentage*100;
+    note.Max = note.Points / note.Percentage * 100;
     note.Average = note.Percentage;
     note.Highest = note.Percentage;
-    note.AllNotes = [note.Percentage];
+    note.Allnotes = [note.Percentage];
 
     flag = true;
 
-    for(j in tree){
-        if(equals(tree[j].Activity[0], note.Activity[0])){ 
+    for (j in tree) {
+        if (equals(tree[j].Activity[0], note.Activity[0])) {
             tree[j].Highest = Math.max(tree[j].Highest, note.Highest);
-            tree[j].AllNotes = tree[j].AllNotes.concat(note.AllNotes);
+            tree[j].Allnotes = tree[j].Allnotes.concat(note.Allnotes);
             console.log(tree[j].Percentage);
-            tree[j].Average = tree[j].AllNotes.reduce((a, b) => a + b, 0) / tree[j].AllNotes.length;
+            tree[j].Average = tree[j].Allnotes.reduce((a, b) => a + b, 0) / tree[j].Allnotes.length;
             flag = false;
             break;
         }
     }
 
-    if(flag){ tree.push(note);}
+    if (flag) {
+        tree.push(note);
+    }
 }
 
 tree = tree.sort(compare);
 
-document.body.innerHTML+="<table><tr> <th>Activity</th> <th>Highest</th> <th>Average</th> <th>Total</th> </tr></table>"
+document.body.innerHTML += "<table><tr> <th>Activity</th> <th>Highest</th> <th>Average</th> <th>Total</th> </tr></table>"
 
-for(i in tree){
+for (i in tree) {
     branch = tree[i];
     html = "<table class='";
 
-    if(branch.Highest>=perfectScore){
-        html+= "green'><tr>";
-    }else if(branch.Highest/2+branch.Average/2 >= 50){
-        html+= "orange'><tr>";
-    }else{
-        html+= "red'><tr>";
+    if (branch.Highest >= perfectScore) {
+        html += "green'><tr>";
+    } else if (branch.Highest / 2 + branch.Average / 2 >= 50) {
+        html += "orange'><tr>";
+    } else {
+        html += "red'><tr>";
     }
 
-    total = branch.Highest/2+branch.Average/2;
+    total = branch.Highest / 2 + branch.Average / 2;
 
-    html += "<td class='left'>"+ branch.Activity[0].join('-') + " " + branch.Activity.slice(1).join(' ') +"</td>";
-    html += "<td class='right'>"+ branch.Highest +"</td>";
-    html += "<td class='right'>"+ branch.Average +"</td>";
-    html += "<td class='right'>"+ total +"</td>";
+    html += "<td class='left'>" + branch.Activity[0].join('-') + " " + branch.Activity.slice(1).join(' ') + "</td>";
+    html += "<td class='right'>" + branch.Highest + "</td>";
+    html += "<td class='right'>" + branch.Average + "</td>";
+    html += "<td class='right'>" + total + "</td>";
 
     html += "</tr></table>";
 
--- read.js
+++ read.js
@@ -1,32 +1,123 @@
-function get(){
-    raw = document.getElementById("input").value.split('\n');
+function get() {
+    let arr = "";
+    while (arr == "") {
+        Array.prototype.slice.call(document.getElementsByClassName("rz-cell-data")).forEach(element => {
+            arr += element.innerHTML;
+        });
+    }
+    raw = arr.split('                <!--!-->\n');
     counter = 0;
     output = [];
-    verifDate = function(line){ return !isNaN(Date.parse(line)) && isNaN(line); }
+    verifDate = function (line) {
+        return !isNaN(Date.parse(line)) && isNaN(line);
+    }
 
-    for(i in raw){
+    for (i in raw) {
         line = raw[i];
-        switch(counter){
+        switch (counter) {
             case 3:
                 output.push({});
-                output[output.length-1].Activity = line.split(' ');
-                counter-= 1;
+                output[output.length - 1].Activity = line.split(' ');
+                counter -= 1;
                 break;
             case 2:
-                output[output.length-1].Percentage = line;
-                counter-= 1;
+                output[output.length - 1].Percentage = line;
+                counter -= 1;
                 break;
             case 1:
-                output[output.length-1].Points = line;
-                counter-= 1;
+                output[output.length - 1].Points = line;
+                counter -= 1;
                 break;
             default:
-                if(verifDate(line)){
+                if (verifDate(line)) {
                     counter = 3;
                 }
         }
     }
-
-    localStorage.setItem("notes", JSON.stringify(output));
-    window.location.href = "output.html";
+    let notes = JSON.stringify(output);
+    chrome.storage.sync.set({
+        "notes": notes
+    }); //if popup version
+    //return jsonformat = JSON.stringify(output); //if direct integration
 }
+
+// notes = JSON.parse(get());
+// tree = []
+
+// perfectScore = 95;
+
+// const equals = (a, b) =>
+// a.length === b.length &&
+// a.every((v, i) => v === b[i]);
+
+// compare = function (a, b) {
+// a = a.Activity[0];
+// b = b.Activity[0];
+
+// for (let i = 0; i < Math.max(a.length, b.length); i++) {
+// if (a[i].localeCompare(b[i]) != 0) {
+// return a[i].localeCompare(b[i]);
+// }
+
+// if (a.length - 1 == i) {
+// return -1;
+// }
+// if (b.length - 1 == i) {
+// return 1;
+// }
+// }
+// }
+
+// for (i in notes) {
+// note = notes[i];
+// note.Percentage = parseInt(note.Percentage)
+// note.Activity[0] = note.Activity[0].split("-");
+// note.Max = note.Points / note.Percentage * 100;
+// note.Average = note.Percentage;
+// note.Highest = note.Percentage;
+// note.AllNotes = [note.Percentage];
+
+// flag = true;
+
+// for (j in tree) {
+// if (equals(tree[j].Activity[0], note.Activity[0])) {
+// tree[j].Highest = Math.max(tree[j].Highest, note.Highest);
+// tree[j].AllNotes = tree[j].AllNotes.concat(note.AllNotes);
+// tree[j].Average = tree[j].AllNotes.reduce((a, b) => a + b, 0) / tree[j].AllNotes.length;
+// flag = false;
+// break;
+// }
+// }
+
+// if (flag) {
+// tree.push(note);
+// }
+// }
+
+// tree = tree.sort(compare);
+
+// // document.body.innerHTML += "<table><tr> <th>Activity</th> <th>Highest</th> <th>Average</th> <th>Total</th> </tr></table>"
+
+// for (i in tree) {
+// branch = tree[i];
+// html = "<table class='";
+
+// if (branch.Highest >= perfectScore) {
+// html += "green'><tr>";
+// } else if (branch.Highest / 2 + branch.Average / 2 >= 50) {
+// html += "orange'><tr>";
+// } else {
+// html += "red'><tr>";
+// }
+
+// total = branch.Highest / 2 + branch.Average / 2;
+
+// html += "<td class='left'>" + branch.Activity[0].join('-') + " " + branch.Activity.slice(1).join(' ') + "</td>";
+// html += "<td class='right'>" + branch.Highest + "</td>";
+// html += "<td class='right'>" + branch.Average + "</td>";
+// html += "<td class='right'>" + total + "</td>";
+
+// html += "</tr></table>";
+
+// document.body.innerHTML += html;
+// }
