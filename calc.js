3f1566003ac643bb0e2ff34154157776e604910d
harkaled2 <80251702+ivan-molnar@users.noreply.github.com>
2022-05-21 18:00:16 +0200

    Adapted new point system

--- calc.js
+++ calc.js
@@ -1,5 +1,172 @@
 let locallyStored = localStorage.getItem("written"); // to verify if notes exist
 let counter = 0;// to count number of loading iteration before interrupt
+let perfectScore = 95;
+
+class Branch{
+    constructor(activity, name, max, root){
+        this.name = name;
+        this.activity = activity.split('-');
+        this.max = max;
+        this.level = this.activity.length
+        this.notes = [];
+        this.highest = -1;
+        this.avg = -1;
+        this.total = -1;
+        this.root = root;
+    }
+
+    getAllNotes(){
+        if(this.level>1){ return this.notes; }
+        else{ return this.notes.concat(root.getAllNotes()); }
+    }
+
+    calcNotes(){
+        let notes = this.getAllNotes();
+        if(notes.length!=0){
+            this.highest = Math.max(...notes);
+            this.avg = notes.reduce((a, b) => (a + b)) / notes.length;
+            this.total = (this.highest/2) + (this.avg/2);
+        }
+    }
+}
+
+function getAllLeavesEmpty(){
+    // branches
+    A1= new Branch("A1", "Analyse", 11500, undefined);
+    A2= new Branch("A2", "Conception", 9100, undefined);
+    A3= new Branch("A3", "Developpement", 21000, undefined);
+    A4= new Branch("A4", "Test", 7000, undefined);
+    A5= new Branch("A5", "Maintenance", 3000, undefined);
+    A6= new Branch("A6", "Management", 8400, undefined);
+    A7= new Branch("A7", "English", 20000, undefined);
+    A8= new Branch("A8", "Soft skills", 20020, undefined);
+
+
+    A1_1= new Branch("A1-1", "Marche", 3500, A1);
+    A1_2= new Branch("A1-2", "Client", 3500, A1);
+    A1_3= new Branch("A1-3", "Cadrage", 4500, A1);
+
+    A2_1= new Branch("A2-1", "Achitecture", 2450, A2);
+    A2_2= new Branch("A2-2", "Ressources", 2450, A2);
+    A2_3= new Branch("A2-3", "Securite", 2625, A2);
+    A2_4= new Branch("A2-4", "Arbitrage", 1575, A2);
+
+    A3_1= new Branch("A3-1", "Tests", 2000, A3);
+    A3_2= new Branch("A3-2", "Code", 11500, A3);
+    A3_3= new Branch("A3-3", "UI", 5000, A3);
+    A3_4= new Branch("A3-4", "Securite", 2500, A3);
+
+    A4_1= new Branch("A4-1", "Test", 5000, A4);
+
+    A7_1= new Branch("A7-1", "Speaking", 6000, A7);
+    //A7-2 is missing from the list of notes
+    A7_3= new Branch("A7-3", "Writing", 6000, A7);
+
+    A8_1= new Branch("A8-1", "Positure", 7150, A8);
+    A8_1_2= new Branch("A8-1-2", "Apprendre", 1430, A8_1);
+    A8_2= new Branch("A8-2", "Communication", 5720, A8);
+    A8_3= new Branch("A8-3", "Resolution de problemes", 7150, A8);
+
+    A3_2_C2= new Branch("A3-2-C2", "Mise en oeuvre", 4500, A3_2);
+
+    // leaves
+    A1_1_C1= new Branch("A1-1-C1", "Besoins", 2250, A1_1);
+    A1_1_C2= new Branch("A1-1-C2", "Valider", 1250, A1_1);
+
+    A1_2_C1= new Branch("A1-2-C1", "Client", 1250, A1_2);
+    A1_2_C2= new Branch("A1-2-C2", "Besoins", 2250, A1_2);
+
+    A1_3_C1= new Branch("A1-3-C1", "Accompagner", 2250, A1_3);
+    A1_3_C2= new Branch("A1-3-C2", "Dossier", 2250, A1_3);
+
+    A2_1_C1= new Branch("A2-1-C1", "Concevoir", 1575, A2_1);
+    A2_1_C2= new Branch("A2-1-C2", "Modeliser", 875, A2_1);
+
+    A2_2_C1= new Branch("A2-2-C1", "Definir", 875, A2_2);
+    A2_2_C2= new Branch("A2-2-C2", "Choisir", 875, A2_2);
+    A2_2_C3= new Branch("A2-2-C3", "Selectionner", 350, A2_2);
+    A2_2_C4= new Branch("A2-2-C4", "Verifier", 350, A2_2);
+
+    A2_3_C1= new Branch("A2-3-C1", "Anticiper", 875, A2_3);
+    A2_3_C2= new Branch("A2-3-C2", "Prototyper", 875, A2_3);
+    A2_3_C3= new Branch("A2-3-C3", "Modeliser", 875, A2_3);
+
+    A2_4_C1= new Branch("A2-4-C1", "Choisir", 1575, A2_4);
+
+    A3_1_C1= new Branch("A3-1-C1", "Rediger", 1000, A3_1);
+    A3_1_C2= new Branch("A3-1-C2", "Indicateurs", 1000, A3_1);
+
+    A3_2_C1= new Branch("A3-2-C1", "Rediger", 4500, A3_2);
+    A3_2_C2_1= new Branch("A3-2-C2-1", "Functional", 1125, A3_2_C2);    
+    A3_2_C2_2= new Branch("A3-2-C2-2", "Imperative", 1125, A3_2_C2);    
+    A3_2_C2_3= new Branch("A3-2-C2-3", "Dynamic", 1125, A3_2_C2);    
+    A3_2_C2_4= new Branch("A3-2-C2-4", "AI", 1125, A3_2_C2);    
+    A3_2_C3= new Branch("A3-2-C3", "Rediger", 2500, A3_2);
+
+    A3_3_C1= new Branch("A3-3-C1", "Interractions", 2500, A3_3);
+    A3_3_C2= new Branch("A3-3-C2", "Responsivite", 2500, A3_3);
+
+    A3_4_C1= new Branch("A3-4-C1", "Deployer", 2500, A3_4);
+
+    A4_1_C1= new Branch("A4-1-C1", "Concevoir", 1000, A4_1);
+    A4_1_C2= new Branch("A4-1-C2", "Mise en oeuvre", 1000, A4_1);
+    A4_1_C3= new Branch("A4-1-C3", "Conformite", 1000, A4_1);
+    A4_1_C4= new Branch("A4-1-C4", "Intrusion", 1000, A4_1);
+    A4_1_C5= new Branch("A4-1-C5", "Jeux d'essais", 1000, A4_1);
+
+    A4_2= new Branch("A4-2", "Mise en production", 2000, A4);
+
+    A5_1= new Branch("A5-1", "Corrective", 1000, A5);
+    A5_2= new Branch("A5-2", "Evolutive", 1000, A5);
+    A5_3= new Branch("A5-3", "Suivi", 1000, A5);
+
+    A6_1= new Branch("A6-1", "Pilotage", 1800, A6);
+    A6_2= new Branch("A6-2", "Veille", 800, A6);
+    A6_3= new Branch("A6-3", "Management", 4800, A6);
+    A6_4= new Branch("A6-4", "Communication", 1000, A6);
+
+    //notation changes from here on out for some reason
+    A7_1_1= new Branch("A7-1-1", "Precision", 2000, A7_1);
+    A7_1_2= new Branch("A7-1-2", "Vocabulaire", 2000, A7_1);
+
+    A7_3_1= new Branch("A7-3-1", "Writing", 2000, A7_3);
+
+    A8_1_1= new Branch("A8-1-1", "Precision", 1430, A8_1);
+    A8_1_2_1= new Branch("A8-1-2-1", "Reading list", 300, A8_1_2);
+
+    A8_2_1= new Branch("A8-2-1", "Oral", 1430, A8_2);
+
+    A8_3_1= new Branch("A8-3-1", "Reformuler", 1430, A8_3);
+    A8_3_3= new Branch("A8-3-3", "Curiosite", 1430, A8_3);
+
+    return [A1_1_C1, A1_1_C2, A1_2_C1, 
+            A1_2_C2, 
+            A1_3_C1, A1_3_C2, 
+            A2_1_C1, A2_1_C2, 
+            A2_2_C1, A2_2_C2, A2_2_C3, A2_2_C4, 
+            A2_3_C1, A2_3_C2, A2_3_C3, 
+            A2_4_C1, 
+            A3_1_C1, A3_1_C2, 
+            A3_2_C1, 
+            A3_2_C2_1, A3_2_C2_2, A3_2_C2_3, A3_2_C2_4, 
+            A3_2_C3, 
+            A3_3_C1, A3_3_C2, 
+            A3_4_C1,
+            A4_1_C1, A4_1_C2, A4_1_C3, A4_1_C4, A4_1_C5, 
+            A4_2, 
+            A5_1, A5_2, A5_3, 
+            A6_1, A6_2, A6_3, A6_4, 
+            A7_1_1, A7_1_2, A7_3_1, 
+            A8_1_1, A8_1_2_1, 
+            A8_2_1, 
+            A8_3_1, A8_3_3
+    ]
+}
+
+function round(num){ 
+    if(num==-1){ return "-"}
+    else{ return Math.round((num + Number.EPSILON) * 100) / 100 }
+}
 
 //function that updates and writes content
 function update(boo) {
@@ -13,7 +180,6 @@ function update(boo) {
 
         let json = localStorage.getItem("note");
         localStorage.removeItem("note");
-        
         // verifying if chrome storage is not empty, if empty either page loaded without the extension installed or just not set because of tab change and so resetting it.
         if (json == "undefined" && counter <= 5){
             tryGet();
@@ -26,107 +192,67 @@ function update(boo) {
         }
 
         //data preparation
-        let notes;
-            notes = JSON.parse(json);
-        tree = []
+        let notes = JSON.parse(json);
 
-        perfectScore = 95;
+        let leaves = getAllLeavesEmpty();
 
-        const equals = (a, b) =>
-            a.length === b.length &&
-            a.every((v, i) => v === b[i]);
-
-        compare = function (a, b) {
-            a = a.Activity[0];
-            b = b.Activity[0];
-
-            for (let i = 0; i < Math.max(a.length, b.length); i++) {
-                if (a[i].localeCompare(b[i]) != 0) {
-                    return a[i].localeCompare(b[i]);
-                }
-
-                if (a.length - 1 == i) {
-                    return -1;
-                }
-                if (b.length - 1 == i) {
-                    return 1;
-                }
-            }
-        }
-
-        for (i in notes) {
-            note = notes[i];
-            note.Percentage = parseInt(note.Percentage)
-            note.Activity[0] = note.Activity[0].split("-");
-            if (note.Percentage != 0) {
-                note.Max = note.Points / note.Percentage * 100;
-            } else {
-                note.Max = 0;
-            }
-            note.Average = note.Percentage;
-            note.Highest = note.Percentage;
-            note.Allnotes = [note.Percentage];
-
-            flag = true;
-
-            for (j in tree) {
-                if (equals(tree[j].Activity[0], note.Activity[0])) {
-                    tree[j].Max = Math.max(tree[j].Max, note.Max);
-                    tree[j].Highest = Math.max(tree[j].Highest, note.Highest);
-                    tree[j].Allnotes = tree[j].Allnotes.concat(note.Allnotes);
-                    tree[j].Average = tree[j].Allnotes.reduce((a, b) => a + b, 0) / tree[j].Allnotes.length;
-                    flag = false;
+        for(note of notes){
+            for(i in leaves){
+                if(leaves[i].activity.join('-').includes(note.Activity[0])){
+                    leaves[i].notes.push(parseInt(note.Percentage));
                     break;
                 }
             }
-
-            if (flag) {
-                tree.push(note);
-            }
         }
 
-        tree = tree.sort(compare);
-
         //pushing data on webpage
-        document.getElementById("outputBody").innerHTML += "<table><tr> <th class='left'>Activity</th> <th class='right'>Highest</th> <th class='right'>Average</th> <th class='right'>Total</th> </tr></table>"
+        html = "";
+        html += "<table><tr> <th class='left'>Activity</th> <th class='right'>Highest</th> <th class='right'>Average</th> <th class='right'>Total</th> </tr></table>"
 
-        lastRoot = tree[0].Activity[0][0]
-        for (i in tree) {
-            html = "";
-            branch = tree[i];
-            if(lastRoot != branch.Activity[0][0]){
-                html += "<div style='height:3px;'></div>";
-                lastRoot = branch.Activity[0][0];
+        lastRoot = leaves[0].activity[0];
+        sum = 0;
+
+        for (leaf of leaves) {
+            leaf.calcNotes();
+            console.log(leaf);
+
+            if(lastRoot != leaf.activity[0]){
+                html += "<div style='height:5px;'></div>";
+                lastRoot = leaf.activity[0];
             }
-            // -- to show off depth difference: --
-            //branchLen = branch.Activity[0].length;
-            //html += "<table style='margin-left:" + (branchLen-1)*20 + "px;' class='";
             html += "<table class='";
 
-
-            if (branch.Highest >= perfectScore) {
+            if(leaf.total==-1){
+                html += "blue'><tr>";
+            }else if (leaf.highest >= perfectScore) {
                 html += "green'><tr>";
-            } else if (branch.Highest / 2 + branch.Average / 2 >= 50) {
+            } else if (leaf.total >= 50) {
                 html += "orange'><tr>";
             } else {
                 html += "red'><tr>";
             }
 
-            total = branch.Highest / 2 + branch.Average / 2;
+            total = leaf.Highest / 2 + leaf.Average / 2;
 
-            html += "<td class='left'>" + branch.Activity[0].join('-') + " " + branch.Activity.slice(1).join(' ') + "</td>";
-            html += "<td class='right'>" + branch.Highest + "</td>";
-            html += "<td class='right'>" + branch.Average + "</td>";
-            html += "<td class='right'>" + total + "</td>";
+            html += "<td class='left'>" + leaf.activity.join('-') + "</td>";
+            html += "<td class='right'>" + round(leaf.highest) + "</td>";
+            html += "<td class='right'>" + round(leaf.avg) + "</td>";
+            html += "<td class='right'>" + round(leaf.total) + "</td>";
 
             html += "</tr></table>";
 
-            document.getElementById("outputBody").innerHTML += html;
+            sum += leaf.total / 100 * leaf.max;
         }
+
+        
+        html += "<table></tr> <td class='end'>Total points: </td> <td class='end'>" + round(sum) + "</td> </tr></table>"
+
+        document.getElementById("outputBody").innerHTML = html;
+
         //saving html so that if popup closed and reopened, no data scraping waiting time
-        localStorage.setItem('written', document.getElementById("outputBody").innerHTML);
+        localStorage.setItem('written', html);
     } else {
-        document.getElementById("outputBody").innerHTML += locallyStored;
+        document.getElementById("outputBody").innerHTML = locallyStored;
     }
 };
 
